58c8a76b969dacaceb6bed36142bdb3f4ff3ccdd
Ginny-Siix <127752611+Ginny-Siix@users.noreply.github.com>
2023-07-22 22:26:00 +0200

    btn radio utilisable

--- scripts/script.js
+++ scripts/script.js
@@ -27,26 +27,42 @@ function lancerJeu() {
   // Initialisations
   let score = 0;
   let i = 0
+  let listeProposition = listeMots
 
   let btnValiderMot = document.getElementById("btnValiderMot")
   let inputEcriture = document.getElementById("inputEcriture")
-  afficherProposition(listeMots[i])
+  afficherProposition(listeProposition[i])
   btnValiderMot.addEventListener("click", () => {
       console.log(inputEcriture.value)
-      if (inputEcriture.value === listeMots[i]) {
+      if (inputEcriture.value === listeProposition[i]) {
           score++
       }
       i++
       afficherResultat(score, i)
       inputEcriture.value = ''
-      if (listeMots[i] === undefined) {
+      if (listeProposition[i] === undefined) {
           afficherProposition("Le jeu est fini, merci d'avoir joué.")
           btnValiderMot.disabled = true
       } else {
-          afficherProposition(listeMots[i])
+          afficherProposition(listeProposition[i])
       }
 
   })
+
+  let listeBtnRadio = document.querySelectorAll(".optionSource input")
+  for (let index = 0; index < listeBtnRadio.length; index++) {
+    listeBtnRadio[index].addEventListener("change", (event) => {
+      console.log(event.target.value)
+      console.log
+      if (event.target.value === "1") {
+        listeProposition = listeMots
+      }else {
+        listeProposition = listePhrases
+      }
+      afficherProposition(listeProposition[i])
+    })
+  }
+
 
 
   afficherResultat(score, i);
